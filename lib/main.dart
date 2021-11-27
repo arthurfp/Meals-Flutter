fc27b051df3039338ab6f8848243c0846c89e718
Arthur Ferreira <fepi.arthur@gmail.com>
2021-11-27 00:00:00 -0300

    added meal details screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'screens/categories_screen.dart';
+import 'screens/tabs_screen.dart';
+// import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
+import 'screens/meal_detail_screen.dart';
 import 'utils/app_routes.dart';
 
 void main() {
@@ -29,8 +31,9 @@ class MyApp extends StatelessWidget {
             ),
       ),
       routes: {
-        AppRoutes.home: (ctx) => const CategoriesScreen(),
+        AppRoutes.home: (ctx) => const TabsScreen(),
         AppRoutes.categoriesMeals: (ctx) => const CategoriesMealsScreen(),
+        AppRoutes.mealDetail: (ctx) => const MealDetailScreen(),
       },
     );
   }
